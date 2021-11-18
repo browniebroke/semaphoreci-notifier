94294db96d479e8bcd09b2becff2aa0af5abd2f4
Bruno Alla <browniebroke@users.noreply.github.com>
2021-11-18 13:50:51 +0000

    feat: make tab active when notification is clicked (#310)

--- source/background.js
+++ source/background.js
@@ -1,8 +1,21 @@
-browser.runtime.onMessage.addListener((message) => {
+browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.type === 'semaphoreci-notifier') {
-    return browser.notifications.create({
+    const creating = browser.notifications.create({
       type: 'basic',
       ...message.options,
+    });
+
+    creating.then((notificationId) => {
+      console.log('Notification created ok: %s', notificationId);
+      const senderTab = sender.tab;
+      browser.notifications.onClicked.addListener((notificationId) => {
+        console.log(
+          'Notification ID=%s was clicked by the user - showing tab ID=%s',
+          notificationId,
+          senderTab.id
+        );
+        browser.tabs.update(senderTab.id, { active: true });
+      });
     });
   }
 });
